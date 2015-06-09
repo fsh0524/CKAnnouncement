e21dfd296014d8536c6a046895c6fd2dffd62b15
fsh0524 <fsh0524@gmail.com>
2015-06-09 13:55:15 +0800

    beta
    beta

---
+++ CKAnnouncement/app/src/main/java/org/entresoft/ckannouncement/FragmentAnn.java
@@ -0,0 +1,22 @@
+package org.entresoft.ckannouncement;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentAnn extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return initView(inflater, container);
+    }
+
+    private View initView(LayoutInflater inflater, ViewGroup container) {
+        View view = inflater.inflate(R.layout.fragment_ann, container, false);
+        return view;
+    }
+}
